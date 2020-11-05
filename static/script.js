a7a8f01211f19e1c9eb26a0a254a3a8f88c5c4fe
Clark Smith <clark.smith79@gmail.com>
2020-11-05 15:20:46 -0700

    added js

--- static/script.js
+++ static/script.js
@@ -1,10 +1,27 @@
 $("#messages").on("click", ".followed", removeFollow);
 $("#messages").on("click", ".not-followed", addFollow);
 
-function removeFollow(evt) {
-  console.log(evt.target);
+async function removeFollow(evt) {
+  console.log("remove");
+  msgId = $(this).data("id");
+  res = await axios.post(`/users/remove_like/${msgId}`);
+  $(this).removeClass("followed btn-primary");
+  $(this).addClass("not-followed btn-secondary");
 }
 
-function addFollow(evt) {
-  console.log(evt.target);
+async function addFollow(evt) {
+  console.log("add");
+  msgId = $(this).data("id");
+  res = await axios.post(`/users/add_like/${msgId}`);
+  $(this).removeClass("not-followed btn-secondary");
+  $(this).addClass("followed btn-primary");
+}
+
+$("#liked-messages").on("click", ".followed", deleteFollow);
+
+async function deleteFollow(evt) {
+  console.log("remove");
+  msgId = $(this).data("id");
+  res = await axios.post(`/users/remove_like/${msgId}`);
+  $(this).remove();
 }
